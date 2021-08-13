c0be7f21ece341b57215963743fbf93c9ec7f00f
Yanyan07 <yenj2015@gmail.com>
2021-08-13 17:48:05 -0600

    comment

---
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.blackjack.app;
+
+public class BlackjackApp {
+
+	public static void main(String[] args) {
+		BlackjackApp bja = new BlackjackApp();
+		bja.run();
+		
+		
+	}
+
+	private void run() {
+		// I need a Dealer
+		// I need a Player
+		// determine the rules of the game
+		
+	}
+
+}
---
+++ src/com/skilldistillery/blackjack/app/Dealing.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.blackjack.app;
+
+public class Dealing {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
